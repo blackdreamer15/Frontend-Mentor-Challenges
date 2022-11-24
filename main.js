f1e64a0a375acc19f49a3169b3e9d902610b0eec
blackdreamer15 <bakijessy@gmail.com>
2022-11-24 23:10:45 +0000

    Import images for destination page into main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import data from './data.json';
+
+
+//======== START OF DESTINATION PAGE SCRIPT ============
+//DESTINATION -- IMAGES IMPORT
+import Moon from '/src/assets/destination/image-moon.webp';
+import Mars from '/src/assets/destination/image-mars.webp';
+import Europa from '/src/assets/destination/image-europa.webp';
+import Titan from '/src/assets/destination/image-titan.webp';
+
+
+
+//======== END OF DESTINATION PAGE SCRIPT ============
