a9e5d0a01a547e6cf41287955a1d77b39b4d32ea
blackdreamer15 <bakijessy@gmail.com>
2022-12-04 19:03:11 +0000

    Get IDs & CLASSEs of destination content in script

--- 8- Space Tourism Website/main.js
+++ 8- Space Tourism Website/main.js
@@ -9,5 +9,18 @@ import europaImg from '/src/assets/destination/image-europa.webp';
 import titanImg from '/src/assets/destination/image-titan.webp';
 
 
+const gotoMoon = document.querySelector('.destination__moon');
+const gotoMars = document.querySelector('.destination__mars');
+const gotoEuropa = document.querySelector('.destination__europa');
+const gotoTitan = document.querySelector('.destination__titan');
+
+const destinationImg = document.querySelector('#destination__img');
+const destinationHeading = document.querySelector('#destination__title');
+const destinationDesc = document.querySelector('#destination__desc');
+const travelDistance = document.querySelector('#destination__distance-in-km');
+const travelDuration = document.querySelector('#destination__travel-duration');
+
+
+
 
 //======== END OF DESTINATION PAGE SCRIPT ============
